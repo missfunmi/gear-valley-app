6f5d3f0e1311953e0ef92d6991f3672cda6da91a
missfunmi <missfunmi@me.com>
2019-12-24 15:58:47 -0500

    Specify request body & enforce content type on request

--- src/main/java/com/gearvalley/application/SearchController.java
+++ src/main/java/com/gearvalley/application/SearchController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +23,8 @@ public class SearchController {
     this.searchService = searchService;
   }
 
-  @PostMapping("")
-  public ResponseEntity<SearchResponse> search(SearchRequest searchRequest) {
+  @PostMapping
+  public ResponseEntity<SearchResponse> search(@RequestBody SearchRequest searchRequest) {
     log.info("Searching with searchRequest={}", searchRequest);
     SearchResponse searchResponse = searchService.search(searchRequest);
     return ResponseEntity.ok(searchResponse);
