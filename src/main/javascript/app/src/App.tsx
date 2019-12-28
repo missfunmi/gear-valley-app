c2755ee23eed615c70c2dc019e05204bf14c1757
charlesmarvin <marvin.charles@gmail.com>
2019-12-28 11:54:59 -0500

    Issue #32 - minor cleanup and refactoring

--- src/main/javascript/app/src/App.tsx
+++ src/main/javascript/app/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import { FontWeights } from '@uifabric/styling'
 import { FontSizes } from '@uifabric/fluent-theme/lib/fluent/FluentType'
-import { Icon } from 'office-ui-fabric-react/lib/Icon'
+import { Link as FabricLink } from 'office-ui-fabric-react/lib/Link'
 import { Stack, IStackTokens } from 'office-ui-fabric-react/lib/Stack'
 import { Text, ITextStyles } from 'office-ui-fabric-react/lib/Text'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom'
 import { SearchPage } from './pages/Search'
 import { WatchesPage } from './pages/Watches'
 
@@ -23,20 +23,29 @@ const stackTokens: IStackTokens = {
   childrenGap: 5,
   padding: 10,
 }
+const Nav: React.FC = () => {
+  const history = useHistory()
+  // manually handling navigation since there is no straight forward way to preserve
+  // fabric theme on links when using react-router Link
+  const handleNavigation = (target: string) => (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    history.push(target)
+  }
+  return (
+    <Stack tokens={stackTokens} horizontal horizontalAlign="space-between">
+      <Link to="/" style={logoLinkStyle}>
+        <Text styles={logoTextStyles}>Gear Valley.</Text>
+      </Link>
+      <FabricLink onClick={handleNavigation('/watches')}>Price Watches</FabricLink>
+    </Stack>
+  )
+}
+
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Stack tokens={stackTokens}>
-            <Link to="/" style={logoLinkStyle}>
-              <Text styles={logoTextStyles}>Gear Valley.</Text>
-            </Link>
-            <Link to="/watches">
-              <Icon iconName="GoToDashboard" />
-            </Link>
-          </Stack>
-        </nav>
+        <Nav />
         <Switch>
           <Route path="/watches">
             <WatchesPage />
